dfe917ca07cf2a084b23531d73cd4a25e4a554ca
AtharvaFTW <atharvavankhede@gmail.com>
2026-06-27 14:28:22 +0530

    debugging

--- src/serving/app.py
+++ src/serving/app.py
@@ -56,7 +56,8 @@ def predict(request: PredictRequest):
         df = df[feature_cols]
         logger.info("Predicting...")
         proba_df = model.predict(df)
-        click_proba = proba_df[1].iloc[0]
+        click_proba = proba_df.iloc[0,0]
+        logger.info(f"Proba df: {click_proba}")
 
         prediction = 1 if click_proba > 0.5 else 0
         logger.info(f"✅ Prediction : {prediction}")
